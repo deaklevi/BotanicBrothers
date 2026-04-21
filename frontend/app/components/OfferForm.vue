4e44bbdf248d64e0e423b86c6238cb9a43a82f30
deaklevi <deakleventevok@gmail.com>
2026-04-21 19:21:42 +0200

    upgrade form.vue

--- frontend/app/components/OfferForm.vue
+++ frontend/app/components/OfferForm.vue
@@ -91,7 +91,7 @@ const handleFileUpload = (event) => {
 
 const submitForm = async () => {
   try {
-    const response = await $fetch('http://localhost:8000/api/contact', {
+    const response = await $fetch('https://botanicbrothers-backend.saavatar.xyz/api/contact', {
       method: 'POST',
       body: {
         lastName: form.value.lastName,
